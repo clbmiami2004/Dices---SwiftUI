c8f1a12ff061ecefc977ac14f0461eb94f67aa9c
Christian Lorenzo <clbmiami2004@gmail.com>
2020-05-19 16:27:46 -0700

    Initial Commit

---
+++ Dices - SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dices - SwiftUI
+//
+//  Created by Christian Lorenzo on 5/19/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
